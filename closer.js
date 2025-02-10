fc8a4006c6b2a8ce7c4c61fe8c47ce7fb0a978e5
Space Pocket <space1985@pocket@gmail.com>
2025-02-10 21:20:45 +0300

    refactor: add loger

--- closer.js
+++ closer.js
@@ -1,10 +1,31 @@
-const createRandomGenerator = (min, max) => {
-  return () => {
-    const rand = min + Math.random() * (max - min);
-    return Math.floor(rand);
+const createLogger = () => {
+    const messagesArr = [];
+  
+    return {
+      log: (message) => {
+        messagesArr.push(message);
+      },
+      getLogs: () => {
+        return messagesArr;
+      },
+    };
   };
-};
+  
+  const myLogger = createLogger();
+  myLogger.log("Первое сообщение");
+  myLogger.log("Второе сообщение");
+  
+  console.log(myLogger.getLogs());
+  
 
-const randomGenerator = createRandomGenerator(1, 5);
-console.log(randomGenerator());
-console.log(randomGenerator());
+const createRandomGenerator = (min, max) => {  
+    return () => {  
+      const rand = min + Math.random() * (max - min);  
+      return Math.floor(rand);  
+    };  
+  };  
+  
+
+  const randomGenerator = createRandomGenerator(1, 5);  
+  console.log(randomGenerator()); 
+  console.log(randomGenerator()); 
